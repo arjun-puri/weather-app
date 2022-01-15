c3e1403ba8f8c7a6b887a82abeb1804a223b333c
Arjun Puri <arjunpuri@arjunpuri.me>
2022-01-15 18:35:27 +0530

    fix for weather icon not rendering.

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React from "react";
 
-const Today = ({temp, pressure, humidity, wind_speed, iconImg, condition}) => {
-    // converts temperature from kelvin to celsius
-    const convertKelvinToCelsius = (tempKelvin) => {
-        return (tempKelvin - 273.15).toFixed(2);
-    }
+const Today = ({ data }) => {
+  const { temp, pressure, humidity, wind_speed, iconImg, condition } = data;
+  // converts temperature from kelvin to celsius
+  const convertKelvinToCelsius = (tempKelvin) => {
+    return (tempKelvin - 273.15).toFixed(2);
+  };
 
-    // converts temperature from kelvin to fahrenheit
-    const convertKelvinToFahrenheit = (tempKelvin) => {
-        return (1.8 * ( (tempKelvin - 273.15) + 32 )).toFixed(2);
-    }
+  // converts temperature from kelvin to fahrenheit
+  const convertKelvinToFahrenheit = (tempKelvin) => {
+    return (1.8 * (tempKelvin - 273.15 + 32)).toFixed(2);
+  };
 
-    // const currTempC = convertKelvinToCelsius(temp);
-    // const currTempF = convertKelvinToFahrenheit(temp);
+  const currTempC = convertKelvinToCelsius(temp);
+  // const currTempF = convertKelvinToFahrenheit(temp);
 
-    return(
+  const otherDetailsRender = ([ detailName, detailValue ]) => {
+    return <li key={detailName}>
+      {detailName}: {detailValue}
+    </li>
+  };
+
+  return (
     <div>
-        <img src={iconImg} alt={condition}/>
-        <span></span>
-        <span></span>
+      <img src={iconImg} alt={condition} />
+      <div>{currTempC}°C</div>
+      <div>
+        <ul>
+          {[
+            [ "Humidity", humidity ],
+            [ "Pressure", pressure ],
+            [ "Wind Speed", wind_speed ],
+          ].map((detail) => otherDetailsRender(detail))}
+        </ul>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Today;
